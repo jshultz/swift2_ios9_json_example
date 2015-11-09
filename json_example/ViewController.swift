15274c6f389721d1826ba92ca95c04ce343e3636
Jason Shultz <jasshultz@gmail.com>
2015-11-09 07:49:11 -0700

    init commit

--- json_example/ViewController.swift
+++ json_example/ViewController.swift
@@ -13,7 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.telize.com/geoip")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            if let urlContent = data {
+                do {
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
+                    
+                    let country = jsonResult["country"]! as! NSString
+                    print(country)
+                } catch {
+                    print("JSON serialization failed")
+                }
+            }
+        }
+        task.resume()
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
